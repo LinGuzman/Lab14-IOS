6b78866b9e5e873936d3602e7d98040ac5376c9a
Lin <lin.guzman@tecsup.edu.pe>
2024-11-06 19:45:25 -0500

    pato 2

---
+++ JSONRESTful/ViewControllerEditarPerfil.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerEditarPerfil.swift
+//  JSONRESTful
+//
+//  Created by Lin Abigail Guzman Gutierrez on 6/11/24.
+//
+
+import UIKit
+
+class ViewControllerEditarPerfil: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
